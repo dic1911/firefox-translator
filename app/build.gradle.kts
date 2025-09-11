aac82358f196687368631f6366cec204765a80fc
dic1911 <d69873@gmail.com>
2025-09-11 21:29:25 +0800

    feat: aidl service for translation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,9 @@ android {
   buildToolsVersion = "34.0.0"
 
   sourceSets {
+    getByName("main") {
+      aidl.srcDir("src/main/aidl")
+    }
     getByName("androidTest") {
       assets {
         srcDirs("src/androidTest/assets")
@@ -76,6 +79,7 @@ android {
     jvmTarget = "11"
   }
   buildFeatures {
+    aidl = true
     compose = true
   }
 }
